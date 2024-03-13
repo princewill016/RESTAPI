23916dcef3ad4a1c07c2735cfb006b4c539cc1a3
Peter David <Princewill016@gmail.com>
2024-03-13 14:15:43 +0100

    building RESTFUL API

---
+++ src/main/java/com/example/student/StudentService.java
@@ -0,0 +1,21 @@
+package com.example.student;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1L,
+                        "david peter",
+                        "princewill016@gmail.com",
+                        LocalDate.of(2000, 05, 22),
+                        21
+
+                ));
+    }
+}
