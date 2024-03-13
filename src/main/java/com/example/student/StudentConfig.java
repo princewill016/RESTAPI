55c160a964e422375ac1035de233bf821d86285a
Peter David <Princewill016@gmail.com>
2024-03-13 18:57:34 +0100

    tested bd with 3 hardcoded info

---
+++ src/main/java/com/example/student/StudentConfig.java
@@ -0,0 +1,37 @@
+package com.example.student;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student Peter = new Student(
+                    "Enotor",
+                    " Epeter2000@yahoo.com",
+                    LocalDate.of(2002, 5, 17),
+                    23);
+            Student Ruth = new Student(
+                    "Omoefe",
+                    " ruth2022@yahoo.com",
+                    LocalDate.of(1967, 8, 17),
+                    23);
+            Student Bright = new Student(
+                    "Ibizugbe",
+                    " Bpeter1999@yahoo.com",
+                    LocalDate.of(1997, 5, 17),
+                    23);
+            repository.saveAll(
+                    List.of(Peter, Ruth, Bright));
+
+        };
+
+    }
+}
--- src/main/java/com/example/student/StudentService.java
+++ src/main/java/com/example/student/StudentService.java
@@ -1,21 +1,20 @@
 package com.example.student;
 
-import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
-    public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "david peter ",
-                        "princewill016@gmail.com",
-                        LocalDate.of(2000, 05, 22),
-                        21
+    @Autowired
+    private final StudentRepository studentRepository;
 
-                ));
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getStudents() {
+        return studentRepository.findAll();
     }
 }
